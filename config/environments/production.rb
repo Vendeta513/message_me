68639329c2f226a6eb01e6a72b923b38481ecb58
Keen Corsiga <keencorsiga@yahoo.com>
2020-09-05 18:53:44 +0800

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://rails-keen-message-me-app.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://rails-keen-message-me-app.herokuapp.com', 'http://rails-keen-message-me-app.herokuapp.com' ]
+  config.action_cable.url = 'wss://keen-message-me-app.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://keen-message-me-app.herokuapp.com', /http:\/\/keen-message-me-app.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

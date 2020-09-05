73758aeed6cd4e3042e8789943d217026d318da2
Keen Corsiga <keencorsiga@yahoo.com>
2020-09-05 15:53:52 +0800

    Change cable.yml action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.web_socket_server_url = "wss://rails-keen-message-me-app.herokuapp.com/cable" 
+  # config.action_cable.allowed_request_origins = ['https://rails-keen-message-me-app.herokuapp.com', 'http://rails-keen-message-me-app.herokuapp.com']
+  config.action_cable.url = 'wss://rails-keen-message-me-app.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://rails-keen-message-me-app.herokuapp.com', 'http://rails-keen-message-me-app.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
